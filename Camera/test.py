f464ac64f2123413efb44a38ea38aa30099c7296
Torben Storm Rasmusssen <113907153+tsrDTU@users.noreply.github.com>
2025-04-29 19:13:46 +0200

    Test tilføjet

--- Camera/test.py
+++ Camera/test.py
@@ -1,4 +1,8 @@
 
-from GetBalls import get_ball_positions, cap
+from GetBalls import get_ball_positions, cap, cv2
 while True:
+#  get_ball_positions.c2.imshow("edges", get_ball_positions.edges)
+  
   print (get_ball_positions())
+  #cv2.imshow("Edges", get_ball_positions().get("edges"))
+  #cv2.imshow("Frame", get_ball_positions().get("frame"))
