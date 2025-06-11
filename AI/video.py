43eaf151fa2754cd9e3a9e76334f646bf672ea64
Torben Storm Rasmusssen <113907153+tsrDTU@users.noreply.github.com>
2025-06-11 12:31:46 +0200

    USB port ændret

--- AI/video.py
+++ AI/video.py
@@ -2,7 +2,9 @@ from ultralytics import YOLO
 import cv2
 import numpy as np
 import math
+
 from sklearn.decomposition import PCA
+
 
 DEBUG = True  # Set to True to enable debug mode
 
@@ -128,7 +130,7 @@ def get_robot_angle(frame):
 # Load your trained model
 #model = YOLO("ball_detect/v3_balls_s_night_run/weights/best.pt")
 model = YOLO("ball_detect/v7/weights/best.pt")
-cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 while True:
     ret, frame = cap.read()
