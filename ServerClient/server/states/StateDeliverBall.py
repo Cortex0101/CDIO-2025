1b8064829a6d4ef88462d758e25691981738a9fe
Cortex0101 <ldeiruff@gmail.com>
2025-06-19 21:06:50 +0200

    Implemented drive to goal

--- ServerClient/server/states/StateDeliverBall.py
+++ ServerClient/server/states/StateDeliverBall.py
@@ -4,14 +4,50 @@ import logging
 logger = logging.getLogger(__name__)
 
 class StateDeliverBall(StateBase):
-    def __init__(self, server):
+    def __init__(self, server, target_object=None): # might not need target_object as its just the closest ball to the robot that should be ignored
         self.server = server  # Reference to the main Server object
 
     def update(self, frame):
-        pass
+        """
+        Update the state with the current frame.
+        This method is called periodically to update the state.
+        """
+        if self.server.course.get_robot() is not None:
+            self.robot_center = self.server.course.get_robot().center
+            self.robot_direction = self.server.course.get_robot().direction
+        else:
+            logger.error("[SERVER] No robot found in the course, using previous position.")
+        
+        frame = self.server.path_planner_visualizer.draw_path(frame, self.server.pure_pursuit_navigator.path)
+        instruction = self.server.pure_pursuit_navigator.compute_drive_command(self.robot_center, self.robot_direction)
+        self.server.send_instruction(instruction)
+        
+        if self._distance(self.robot_center, self.server.pure_pursuit_navigator.path[-1]) < 10:
+            logger.info("[SERVER] Reached the end of the path.")
+            self.server.pure_pursuit_navigator.set_path(None)
+            instruction = {"cmd": "drive", "left_speed": 0, "right_speed": 0}
+            self.server.send_instruction(instruction)
 
     def on_enter(self):
-        pass
+        self.GOAL_LOCATION = (200, 200)  # HARD-CODED GOAL LOCATION, replace with self.course.get_goals find the closest
+        self.robot = self.server.course.get_robot()
+        if self.robot is None:
+            logger.error("[SERVER] No robot found in the course, returning to idle state.")
+            from .StateIdle import StateIdle
+            self.server.set_state(StateIdle(self.server))
+        self.robot_direction = self.robot.direction()
+        self.robot_center = self.robot.center
+        
+        self.target_location = self.server.course.get_optimal_goal_parking_spot(self.GOAL_LOCATION)
+        excluded_ball = self.server.course.get_nearest_ball(self.course.get_robot().center, 'either')
+        current_path = self.server.path_planner.find_path(self.robot_center, self.target_location, self.path_planner.generate_grid(self.course, excluded_objects=[excluded_ball] if excluded_ball is not None else []))
+        if current_path is not None and len(current_path) > 0:
+            self.server.pure_pursuit_navigator.set_path(current_path)
+            logger.info(f"[SERVER] Path found to deliver the ball: {len(current_path)} points.")
+        else:
+            logger.error("[SERVER] No path found to deliver the ball.")
+            from .StateIdle import StateIdle
+            self.server.set_state(StateIdle(self.server))
 
     def on_exit(self):
         pass
